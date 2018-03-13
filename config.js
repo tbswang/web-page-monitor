953db51a22d1cd61f0778e0fed1e925941516a2d
gt2017 <gt2017qd@163.com>
2018-03-13 23:13:57 +0800

    add getHtmlResult func

---
+++ config.js
@@ -0,0 +1,9 @@
+config = {
+    url: 'http://ncms.ustb.edu.cn',
+    content: {
+        md5: '5f65597dfea5639327cafe2f1fd64fc3',
+        time: 'Mon Mar 12 2018 23:20:39 GMT+0800'
+    }
+}
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-const schedule = require('node-schedule');
+//modules needed
 const http = require('http');
+const crypto = require('crypto');
+const schedule = require('node-schedule');
 
+// get url from config.js
+const config = require('./config.js');
+const url = config.url;
 
+let md5 = crypto.createHash('md5');
+
+//get the content of html page
+function getHtmlResult() {
+    http.get(url, (res) => {
+        const { statusCode } = res;
+        let err;
+        if (statusCode != 200) {
+            err = new Error('请求失败.\n' + `状态码:${statusCode}`);
+        }
+        if (err) {
+            console.log(err.message);
+            res.resume();
+            return;
+        }
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => {
+            rawData += chunk;
+        });
+        res.on('end', () => {
+            try {
+                let htmlMd5 = md5.update(rawData).digest('hex');
+                console.log(Date());
+                console.log(htmlMd5);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    })
+}
+
+getHtmlResult();
